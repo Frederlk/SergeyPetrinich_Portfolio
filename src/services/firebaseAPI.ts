5e39e5f96d1d0e09c575cd57386f832c8442da7d
Petrinich Sergey <petrinich.sergey@gmail.com>
2024-09-16 15:59:51 +0300

    chore: redeployed

--- src/services/firebaseAPI.ts
+++ src/services/firebaseAPI.ts
@@ -25,3 +25,23 @@ export const firebaseAPI = createApi({
         }),
     }),
 });
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     // apiKey: "",
+//     // authDomain: "",
+//     // projectId: "",
+//     // storageBucket: "",
+//     // messagingSenderId: "",
+//     // appId: "",
+//     // measurementId: "",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const db = getFirestore(app);
+// export const storage = app.storage();
